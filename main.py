1ddf2be2ec46f0b0375353d49a20182534f63707
Jean Marc Nadeau Huamani <101827583+Jean-Marc-NH@users.noreply.github.com>
2023-04-27 12:18:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-"""
-Proyecto:
-- Abrir asistente
-- Login o registro
-- regitro va a crear un usuario en la bd
-- login lo va a identificar 
-- despues preguntara si queremos crear, una nota mostrarlas o borrarlas
-"""
-from os import system
 import dependencias.programa as p
 
 p.programa()
